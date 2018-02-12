381f2abd4d1b1ac0cfb99752df21d85e56153e19
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-02-12 16:10:25 -0500

    Able to grab the word to obfuscate

--- pwd.js
+++ pwd.js
@@ -4,8 +4,16 @@
 // I'll write a program that can obfuscate a word
 // I don't know how yet.
 
+console.log("About to start");
 // Step 1: get word
+
+var arguments = process.argv
+console.log("Arguments: ",arguments);
+
 // step 2: put it in a variable
+var word = arguments[2];
+console.log("Obfuscating the word: ", word);
+
 // step 3: look at each letter
 // step 4: if letter is E replace with 3
 // step 5: print out result

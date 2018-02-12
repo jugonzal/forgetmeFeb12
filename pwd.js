fb12ebde07a6a570e2e9911bd3ebbcea9aa89dee
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-02-12 16:17:31 -0500

    Done. writing word in a column

--- pwd.js
+++ pwd.js
@@ -4,17 +4,24 @@
 // I'll write a program that can obfuscate a word
 // I don't know how yet.
 
-console.log("About to start");
+// console.log("About to start");
 // Step 1: get word
 
 var arguments = process.argv
-console.log("Arguments: ",arguments);
+// console.log("Arguments: ",arguments);
 
 // step 2: put it in a variable
 var word = arguments[2];
-console.log("Obfuscating the word: ", word);
+// console.log("Obfuscating the word: ", word);
 
 // step 3: look at each letter
-// step 4: if letter is E replace with 3
+for (i=0; i<word.length; i++) {
+  // step 4: if letter is E replace with 3
+  if (word[i] == 'e') {
+    console.log("3");
+  } else {
+    console.log(word[i])
+  }
+}
 // step 5: print out result
 

213675cc4bfed75f237db968d55cee6063e280fa
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-02-12 16:00:02 -0500

    Just figured out the general solution

---
+++ pwd.js
@@ -0,0 +1,12 @@
+// impossibly difficult command line tool to master: vi
+// or a much easier graphical text editor: sublime
+
+// I'll write a program that can obfuscate a word
+// I don't know how yet.
+
+// Step 1: get word
+// step 2: put it in a variable
+// step 3: look at each letter
+// step 4: if letter is E replace with 3
+// step 5: print out result
+
